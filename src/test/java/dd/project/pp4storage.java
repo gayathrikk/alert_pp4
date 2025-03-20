bf87b50d80a7d460b55f45172448af75165f6e43
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2025-03-20 15:44:48 +0530

    pp4storage.java

--- src/test/java/dd/project/pp4storage.java
+++ src/test/java/dd/project/pp4storage.java
@@ -91,10 +91,9 @@ public class pp4storage {
 
 	 private void sendEmailAlert(String messageBody) {
 	        // Recipient's email ID needs to be mentioned.
-	  
-		 String[] to = {"nathan.i@htic.iitm.ac.in"};
-	    	String[] cc = {"venip@htic.iitm.ac.in"};
-	        String[] bcc = {"divya.d@htic.iitm.ac.in"};   
+	  String[] to = {"nathan.i@htic.iitm.ac.in"};
+        String[] cc = {"venip@htic.iitm.ac.in"};
+        String[] bcc = {"divya.d@htic.iitm.ac.in"};
 	    
 	        // Sender's email ID needs to be mentioned
 	        String from = "gayathri@htic.iitm.ac.in";
